f7e8db237469eb3efd3b2beea76ff2f5110658a4
yassun <yasukun.taga@gmail.com>
2024-04-15 06:31:24 +0900

    add embeddings column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_11_214904) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_11_215904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "vector"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_11_214904) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.vector "embedding", limit: 1024
   end
 
 end
