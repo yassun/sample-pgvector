79f7e746c17a2a5d9af79fab16319307351d2605
yassun <yasukun.taga@gmail.com>
2024-04-16 11:28:01 +0900

    add embeddings endpoint

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "index page"
+
+@app.route('/api/embeddings', methods=['POST'])
+def api_embeddings():
+    sentences = request.form["data"]
+    embeddings = SentenceTransformer('intfloat/multilingual-e5-large').encode(sentences)
+    return jsonify({"embeddings": embeddings.tolist()})
